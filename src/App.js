43be964164a425e09cef5826d7c118c84f16e97d
ardhisparahita <92034363+ardhisparahita@users.noreply.github.com>
2022-11-01 22:02:58 +0700

    refactor app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React, { useState } from 'react'
-import CardList from './Components/CardList'
-import { listRobots } from './utils/robots';
-import SearchBox from './Components/SearchBox';
+import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
-  // eslint-disable-next-line
-  const [robots, setRobots] = useState(listRobots)
-  const [searchField, setSearchField] = useState("")
-
-  function onSearchChange(event) {
-    setSearchField(event.target.value)
-    
-  }
-  const filteredRobots = robots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchField.toLowerCase())
-  })
   return (
     <>
     <BrowserRouter>
@@ -30,11 +17,7 @@ const App = () => {
         <Route
           path='/:token'
           element={
-            <div className='tc'>
-            <h1>Robofriend</h1>
-            <SearchBox searchChange={onSearchChange}/>
-            <CardList robots={filteredRobots}/>
-            </div>
+            <Home/>
           }
         />
     </Routes>
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import CardList from '../Components/CardList'
+import { listRobots } from '../utils/robots';
+import SearchBox from '../Components/SearchBox';
+import { useParams } from 'react-router-dom';
+import axios from 'axios'
+import { useEffect } from 'react';
+
+const Home = () => {
+  const{token} = useParams
+  const[data, setData] = useState()
+
+  const getData = async () => {
+    const {data} = await axios("https://fadfaasd.herokuapp.com/api/v1/profile", {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    setData(data)
+  }
+
+  useEffect(() => {
+    getData()
+    // eslint-disable-next-line
+  }, [])
+
+  // eslint-disable-next-line
+  const [robots, setRobots] = useState(listRobots)
+  const [searchField, setSearchField] = useState("")
+  function onSearchChange(event) {
+    setSearchField(event.target.value)
+  }
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+
+  return (
+    <div className='tc'>
+      <h1>Selamat datang, {data?.data.email}</h1>  
+      <h1>Robofriend</h1>
+      <SearchBox searchChange={onSearchChange}/>
+      <CardList robots={filteredRobots}/>
+    </div>
+  )
+}
+
+export default Home
