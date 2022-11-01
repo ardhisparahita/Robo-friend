10e4c050e6eefad6f47e4b250d62c9fdb3ae0d3c
ardhisparahita <92034363+ardhisparahita@users.noreply.github.com>
2022-11-01 21:25:28 +0700

    add element

--- src/App.js
+++ src/App.js
@@ -23,18 +23,20 @@ const App = () => {
       <Route
         path='/'
         element={
-          <>
           <div className="App">
             <a href='https://fadfaasd.herokuapp.com/api/v1/auth/google'>Login Google Ardhis Parahita</a>
           </div>
-          <div className='tc'>
-          <h1>Robofriend</h1>
-          <SearchBox searchChange={onSearchChange}/>
-          <CardList robots={filteredRobots}/>
-        </div>
-        </>
         }/>
-        
+        <Route
+          path='/home'
+          element={
+            <div className='tc'>
+            <h1>Robofriend</h1>
+            <SearchBox searchChange={onSearchChange}/>
+            <CardList robots={filteredRobots}/>
+            </div>
+          }
+        />
     </Routes>
     </BrowserRouter>
     
